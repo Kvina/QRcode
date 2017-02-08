9d99c089c1c160f9b5e843039cf032d65102ea1c
yangminyu <yangminyu@gmail.com>
2017-02-08 16:26:49 +0800

    change
    test

--- src/com/google/zxing/Writer.java
+++ src/com/google/zxing/Writer.java
@@ -23,6 +23,7 @@ import java.util.Map;
 /**
  * The base class for all objects which encode/generate a barcode image.
  *
+ 
  * @author dswitkin@google.com (Daniel Switkin)
  */
 public interface Writer {
